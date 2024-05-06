0410cffad1126126f3b46feb42905ac5772ee656
Abdo <83555913+abdoibrahim257@users.noreply.github.com>
2024-05-06 18:44:31 +0300

    chat logic finished

--- gp-website/src/App.js
+++ gp-website/src/App.js
@@ -4,18 +4,20 @@ import './App.css'; //here this css is the global css file for the whole website
 import LandingPage from './components/landingPage';
 import NavBar from './components/navbar';
 import SymptomPage from './components/symptomPage';
+import ChatPage from './components/ChatPage';
 
 
 function App() {
   return (
     <>
       {/* here we will handle all routes AKA the biggest component */}
-      <Routes>
+      {/* <Routes>
         <Route path="/" element={<LandingPage />} />
 
         <Route path="/maven" element={<SymptomPage />} />
 
-      </Routes>
+      </Routes> */}
+      <ChatPage />
     </>
   );
 }

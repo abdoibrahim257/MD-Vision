b4d01e0fa244ed5e1d4c6dc0a9bdc050ac224c5f
AmrAhmed412 <75176768+AmrAhmed412@users.noreply.github.com>
2024-05-14 17:59:59 +0300

    refactoring some code

--- kengic-main/src/Our/main.py
+++ kengic-main/src/Our/main.py
@@ -3,7 +3,7 @@ from utils import *
 def main():
     print('AMERICA YA, HALOOO')
     caption_gen = initialize()
-    caption_gen.caption_generation()
+    # caption_gen.caption_generation()
     
 
 def initialize():
@@ -15,11 +15,13 @@ def initialize():
     max_iters = 150 #max iterations default = 150
     configs = [n, parents, hops, nlogP, optimiser, max_iters]
     start_end_tokens = ['<t>', '</t>']
-    out_folder = 'output_captions'
-    input_csv_file_path = './input.csv' #CNN output will be here
+    out_folder = 'output_captions'  #CNN output will be here
+    input = pd.read_csv('./input.csv')
+    img_id = input['img_id'].values[0]
+    data,ngram_df,references = load_data(img_id,n)
 
-    caption_gen = kengic(configs, out_folder, input_csv_file_path)
-    return caption_gen
+    # caption_gen = kengic(configs, out_folder)
+    # return caption_gen
 
 main()
     
